a8b1bb2ae9076c3d7f24a61f582d79b2dfe0a5e0
Colin Arel <romans_one16@live.com>
2020-10-06 14:35:25 -0500

    add animal button SUCCESS

--- src/components/animal/AnimalList.js
+++ src/components/animal/AnimalList.js
@@ -2,10 +2,14 @@ import React, { useContext, useEffect } from "react"
 import { AnimalContext } from "./AnimalProvider"
 import { AnimalCard } from "./AnimalCard"
 import "./Animal.css"
+import { useHistory } from 'react-router-dom';
+
 
 export const AnimalList = () => {
     // This state changes when `getAnimals()` is invoked below
     const { animals, getAnimals } = useContext(AnimalContext)
+
+    const history = useHistory()
 
     //useEffect - reach out to the world for something
     useEffect(() => {
@@ -14,15 +18,20 @@ export const AnimalList = () => {
 
     }, [])
 
-
     return (
-        <div className="animals">
-            {console.log("AnimalList: Render")}
-            {
-                animals.map(animal => {
-                    return <AnimalCard key={animal.id} location={animal.location.name} animal={animal} />
-                })
-            }
-        </div>
+        <>
+            <h2>Animals</h2>
+            <button onClick={() => { history.push("/animals/create") }}>
+                Add Animal
+            </button>
+            <div className="animals">
+                {console.log("AnimalList: Render")}
+                {
+                    animals.map(animal => {
+                        return <AnimalCard key={animal.id} location={animal.location.name} animal={animal} />
+                    })
+                }
+            </div>
+        </>
     )
 }
